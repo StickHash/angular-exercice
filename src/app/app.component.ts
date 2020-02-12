f400669098a1bd705f3e0f4288086d3572c1e359
Christophe BELBEOCH <christophe.belbeoch2019@campus-eni.fr>
2020-02-12 15:48:53 +0100

    Final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FirstProject';
+export class AppComponent implements OnInit, OnDestroy {
+  lastUpdate = new Date();
+  secondes: number;
+  counterSubscription: Subscription;
+
+
+  ngOnInit(): void {
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
---
+++ src/app/services/appareil.service.ts
@@ -0,0 +1,89 @@
+import {Subject} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class AppareilService {
+
+  appareilsSubject = new Subject<any[]>();
+
+  private appareils = [
+  ];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  emitAppareilSubject() {
+    this.appareilsSubject.next(this.appareils.slice());
+  }
+
+  switchOnAll() {
+    for (const appareil of this.appareils) {
+      appareil.status = 'allumé';
+    }
+  }
+
+  switchOffAll() {
+    for (const appareil of this.appareils) {
+      appareil.status = 'éteint';
+    }
+  }
+
+  switchOnOne(i: number) {
+    this.appareils[i].status = 'allumé';
+  }
+
+  switchOffOne(i: number) {
+    this.appareils[i].status = 'éteint';
+  }
+
+  getAppareilById(id: number) {
+    const appareil = this.appareils.find(
+      (s) => {
+        return s.id === id;
+      }
+    );
+    return appareil;
+  }
+
+  addAppareil(name: string, status: string) {
+    const appareilObject = {
+      id: 0,
+      name: '',
+      status: ''
+    };
+    appareilObject.name = name;
+    appareilObject.status = status;
+    appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
+    this.appareils.push(appareilObject);
+    this.emitAppareilSubject();
+  }
+
+  saveAppareilsToServer() {
+    this.httpClient
+      .put('https://first-project-bdd.firebaseio.com/appareils.json', this.appareils)
+      .subscribe(
+        () => {
+          console.log('Enregistrement terminé !');
+        },
+        (error) => {
+          console.log('Erreur ! : ' + error);
+        }
+      );
+  }
+
+  getAppareilsFromServer() {
+    this.httpClient
+      .get<any[]>('https://first-project-bdd.firebaseio.com/appareils.json')
+      .subscribe(
+        (response) => {
+          this.appareils = response;
+          console.log(this.appareils);
+          this.emitAppareilSubject();
+        },
+        (error) => {
+          console.log('Erreur ! : ' + error);
+        }
+      );
+  }
+}
